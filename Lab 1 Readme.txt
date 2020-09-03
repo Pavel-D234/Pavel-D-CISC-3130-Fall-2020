520cf5bc184fb2d853bb7489c88d0d9a788f4e33
Pavel-D234 <70726979+Pavel-D234@users.noreply.github.com>
2020-09-03 18:52:16 -0400

    Add files via upload

---
+++ Lab 1 Readme.txt
@@ -0,0 +1,3 @@
+This is my Lab 1 assignment.
+It is a simple Hello World style program written in Java.
+It outputs three lines of text to standard output.
---
+++ Lab1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pavel.d.cisc.pkg3130.fall.pkg2020;
+
+/**
+ *
+ * @author My PC
+ */
+public class Lab1{
+  public static void main (String[] args){
+    System.out.println("Hello Professor");
+    System.out.println("This is Pavel.");
+    System.out.println("This is my Lab 1 assignment.");
+    }
+}
